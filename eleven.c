75549bb7c0870693acb97b822f95194734266098
SurendranSuru <surendran1312@gmail.com>
2016-07-10 11:21:38 +0530

    Create eleven.c

---
+++ eleven.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,x,p,i;
+printf("Enter the child");
+scanf("%d",&a);
+printf("Pairs");
+scanf("%d",&p);
+for(i=0;i<2*a;i++)
+{
+x=2*a-i;
+if(p!=x)
+{
+printf("The pair is%d for days %d is %d\n",p,j++,p);
+}
+}
+}
