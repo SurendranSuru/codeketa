382d398a6fe19cec3e90e918c54d780a7ac39ba6
SurendranSuru <surendran1312@gmail.com>
2016-09-02 13:48:00 +0530

    Create pn.c

---
+++ pn.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n=0;
+if(n>0)
+{ 
+printf("Positive number");
+else
+{
+if(n==0)
+{
+printf("Zero");
+else
+printf("Negative number");
+}}}
